2bbbf95a9e1a2a3d483fc213c7de577b30a08330
elandau91 <eli.b.landau@gmail.com>
2020-07-27 20:40:22 -0400

    basic search page built

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,30 @@
+Tmdb::Api.key("a9906c88d9a679228d60881374c08beb")
+Tmdb::Api.language("en")
+
 class MoviesController < ApplicationController
+
+    def search
+        @query = params[:query]
+
+        @results = get_results(@query) if @query && !@query.empty?
+
+        render :search
+    end
+
+
+
+
+
+
+    private
+
+    def get_results (query)
+        @search = Tmdb::Search.new
+        @search.resource('movie') 
+        @search.query(query) 
+        @movie = @search.fetch 
+
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :reviews, only: [:new, :create, :edit, :update, :destroy]
   resources :movies
   resources :users
+
+  get "/search", to: "movies#search", as: "movies_search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
