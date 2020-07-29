4b5b34c41b946c71fd7f4168df6779b5fa3570d7
Aiannn <soktoevayan@gmail.com>
2020-07-29 16:07:03 -0400

    update, delete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -20,6 +20,8 @@ class MoviesController < ApplicationController
             @api_movie = Tmdb::Movie.detail(params[:id])
             @movie = Movie.new(title: @api_movie["original_title"], poster: @api_movie["poster_path"], release_date: @api_movie["release_date"])
         end
+    
+        @review = Review.find_or_create_by(user_id: @current_user.id, movie_id: @movie.id)
     end
 
 
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,12 +6,25 @@ class ReviewsController < ApplicationController
     end
     #need refactoring!! (with strong params)
     def create 
-        Review.create(user_id: @current_user.id, movie_id: params[:review][:movie_id], rating: params[:review][:rating], content: params[:review][:content])
+        Review.create(user_id: @current_user.id, movie_id: params[:review][:movie_id], rating: params[:review][:rating], content: params[:review][:content], favorite: params[:review][:favorite])
         redirect_to user_path(@current_user)
     end
 
-    def add_to_favorite
-        
+    def edit
+        @movies = Movie.all
+        @review = Review.find_by(id: params[:id])
+    end 
+
+    def update
+        @review = Review.find_by(id: params[:id])
+        @review.update(user_id: @current_user.id, movie_id: params[:review][:movie_id], rating: params[:review][:rating], content: params[:review][:content])
+        redirect_to user_path(@current_user)
+    end
+
+    def destroy
+
+        @review = Review.find_by(id: params[:id]).destroy 
+        redirect_to user_path(@current_user)
     end
 
     private
